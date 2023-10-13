db5f154e7a2bd57fc5921a322249b4caa55b0d6c
flashrifle <flashrifle@gmail.com>
2023-10-13 14:05:12 +0900

    feat: todo 수정

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -2,12 +2,40 @@ import React, { useState } from 'react';
 
 export default function Todo({ item, deleteItem }) {
     console.log('item : ', item);
-    const { id, title, done } = item;
-
     const [todoItem, setTodoItem] = useState(item);
+    const [readOnly, setReadOnly] = useState(true);
+
+    const { id, title, done } = item;
     const deleteTodo = (value) => {
         deleteItem(todoItem);
     };
+
+    const offReadOnlyMode = () => {
+        setReadOnly(false);
+    };
+
+    const editEventHandler = (e) => {
+        const { title, ...rest } = todoItem;
+        setTodoItem({
+            title: e.target.value,
+            ...rest,
+        });
+    };
+
+    const editKeyEventHandler = (e) => {
+        if (e.key === 'Enter') {
+            setReadOnly(true);
+        }
+    };
+
+    const checkboxEventHandler = (e) => {
+        const { done, ...rest } = todoItem;
+        setTodoItem({
+            done: e.target.checked,
+            ...rest,
+        });
+    };
+
     return (
         <div>
             <input
@@ -15,8 +43,17 @@ export default function Todo({ item, deleteItem }) {
                 name={`todo${id}`}
                 id={`todo${id}`}
                 defaultChecked={done}
+                onChange={checkboxEventHandler}
             />
-            <label htmlFor={`todo${id}`}>{title}</label>
+            {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+            <input
+                type="text"
+                value={todoItem.title}
+                readOnly={readOnly}
+                onClick={offReadOnlyMode}
+                onChange={editEventHandler}
+                onKeyDown={editKeyEventHandler}
+            />
             <button onClick={deleteTodo}>DELETE</button>
         </div>
     );
